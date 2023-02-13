23142efe150556a3c00cf2b0dca4cf80683caf2e
Guilherme Oliveira <coliveira.guilherme@gmail.com>
2023-02-13 19:49:34 -0300

    refactor: add boolean return to verificaChute function

--- js/validacao.js
+++ js/validacao.js
@@ -2,15 +2,15 @@ function verificaChute(chute) {
     const numero = +chute
 
     if (chuteInvalido(numero)) {
-        elementoChute.innerHTML += '<div>Valor inválido</div>'
-        return
+        el_chute.innerHTML += '<div>Valor inválido</div>'
+        return false
     }
 
     if (numeroForaDoLimite(numero)) {
-        elementoChute.innerHTML += `
+        el_chute.innerHTML += `
         <div>Valor inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>
         `
-        return
+        return false
     }
 
     if (numero === numeroSecreto) {
@@ -21,15 +21,16 @@ function verificaChute(chute) {
             <button id="jogar-novamente" class="btn-jogar"><i class="fa-solid fa-rotate-left"></i></button> 
         `
     } else if (numero > numeroSecreto) {
-        elementoChute.innerHTML += `
+        el_chute.innerHTML += `
         <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
         `
     } else {
-        elementoChute.innerHTML += `
+        el_chute.innerHTML += `
         <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
         `
     }
- 
+
+    return true
 }
 
 function chuteInvalido(numero) {
@@ -41,8 +42,6 @@ function numeroForaDoLimite(numero) {
 }
 
 document.body.addEventListener('click', e => {
-    console.log(e.target.parentNode.id)
-    if( e.target.parentNode.id == 'jogar-novamente') {
+    if(e.target.parentNode.id == 'jogar-novamente')
         window.location.reload()
-    }
 })
